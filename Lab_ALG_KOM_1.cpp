00f46a46ee88ff878e61f17848abc0ac697b7f64
hp <hp@DESKTOP-MKRDLLA>
2025-10-22 18:14:48 +0700

    Добавьте файлы проекта.

---
+++ Lab_ALG_KOM_1.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream> 
+#include "MyLib.h"
+using namespace std;
+
+int main() {
+    cout << "Traveling Salesman Problem \n";
+    int** matrix = nullptr;  
+    int Cities = 0, k = 0;
+
+    cout << "How many cities: ";
+    cin >> Cities;
+
+    // Проверка корректности ввода
+    if (Cities <= 0) {
+        cout << "Error: Number of cities must be positive!" << endl;
+        return 1;
+    }
+
+    // Выделение памяти для матрицы
+    matrix = new int* [Cities];
+    for (int i = 0; i < Cities; i++) {
+        matrix[i] = new int[Cities]();
+    }
+
+    Randmatr(matrix, Cities, Cities);
+    Outputmatr(matrix, Cities, Cities);
+
+    cout << endl << "Enter city number: ";
+    cin >> k;
+
+    // Проверка корректности номера города
+    while (k <= 0 || k > Cities) {
+        cout << "\nWe have only " << Cities << " cities (1 to " << Cities << ")\nEnter city number: ";
+        cin >> k;
+    }
+
+    int total = 1;  // Подсчет количества перестановок
+    for (int i = 1; i < Cities; i++)
+        total *= i;
+    cout << endl << "Number of permutations: " << total << endl;
+
+    int* perest = new int[Cities + 1]();
+    int* PerestMin = new int[Cities + 1](); 
+
+    Peres(perest, Cities, k);
+    OutputPeres(perest, Cities);
+
+    /*cout << "Permutation: ";
+    for (int i = 0; i <= Cities; i++) {
+        cout << perest[i] << " ";
+    }
+    cout << endl;*/
+
+    int minSumPermut = PermutationSum(matrix, perest, Cities);  // Минимальная сумма маршрута
+
+    cout << " - Route length number 1: " << minSumPermut << endl;
+    PerestMin[Cities] = k;
+    for (int i = 0; i < Cities; i++)
+        PerestMin[i] = perest[i];  // Сохраняем первую перестановку как минимальная
+
+    int a = 2;
+    while (Permutation(perest, Cities) != 0) {
+        int flag = PermutationSum(matrix, perest, Cities);
+        if (flag < minSumPermut) {
+            minSumPermut = flag;  // Сохраняем минимальную сумму перестановки
+            PerestMin[Cities] = k;  // Последний элемент - начальный город
+            for (int i = 0; i < Cities; i++)  // Сохраняем мин перестановку
+                PerestMin[i] = perest[i];
+        }
+        cout << " - Route length number " << a++ << ": " << flag << endl;
+    }
+
+    cout << endl;
+    OutputPeres(PerestMin, Cities);
+    cout << endl << "Length of the minimum rout: " << minSumPermut;
+
+    // Освобождение памяти
+    delete[] perest;
+    delete[] PerestMin;
+    for (int i = 0; i < Cities; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+
+    return 0;
+}
---
+++ MyLib.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// 1. Обмен двух элементов
+void Swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+// 2. Создание матрицы расстояний
+void Randmatr(int** matr, int strok, int stolb) {
+    srand(static_cast<unsigned int>(time(0)));
+    for (int i = 0; i < strok; i++) {
+        for (int j = 0; j < stolb; j++) {
+            if (i == j)
+                matr[i][j] = 0;
+            else
+                matr[i][j] = rand() % 15 + 1;
+        }
+    }
+}
+
+// 3. Вывод матрицы
+void Outputmatr(int** matr, int strok, int stolb) {
+    cout << "Matrix:\n";
+    for (int i = 0; i < strok; i++) {
+        for (int j = 0; j < stolb; j++) {
+            cout << matr[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+}
+
+// 4. Создание первой перестановки
+void Peres(int* arr, int size, int k) {
+    arr[0] = k;
+    arr[size] = k;
+    for (int i = 1, t = 1; i < size; i++, t++) {
+        if (t == k)
+            t++;
+        arr[i] = t;
+    }
+}
+
+// 5. Вывод перестановки
+void OutputPeres(int* P, int cities) {
+    for (int i = 0; i < cities; i++) {
+        cout << P[i] << " ";
+    }
+    cout << P[0];  // Показываем возврат в начальный город
+}
+
+// 6. Реверс хвоста перестановки
+void Inversion(int* P, int cities, int imax) {
+    for (int j = cities - 1, i = imax + 1; i < j; i++, j--) {
+        Swap(&P[i], &P[j]);
+    }
+}
+
+// 7. Генерация следующей перестановки
+bool Permutation(int* P, int cities) {
+    int imax = -1, jmax = -1;
+
+    // Поиск индекса для обмена
+    for (int i = 1; i < cities - 1; i++) {
+        if (P[i] < P[i + 1])
+            imax = i;
+    }
+    if (imax == -1) return false;
+
+    // Поиск элемента для обмена
+    for (int j = imax + 1; j < cities; j++) {
+        if (P[imax] < P[j])
+            jmax = j;
+    }
+
+    // Обмен и реверс
+    Swap(&P[imax], &P[jmax]);
+    Inversion(P, cities, imax);
+    return true;
+}
+
+// 8. Вычисление длины маршрута
+int PermutationSum(int** Matr, int* P, int cities) {
+    int total = 0;
+    for (int i = 0; i < cities; i++) {
+        int from = P[i] - 1;  
+        int to = P[i + 1] - 1;
+
+        // Проверка выхода за границы (на всякий случай)
+       /* if (from < 0 || from >= cities || to < 0 || to >= cities) {
+            cout << "ERROR: Invalid city index!" << endl;
+            return -1;
+        }*/
+
+        total += Matr[from][to];
+    }
+    return total;
+}
